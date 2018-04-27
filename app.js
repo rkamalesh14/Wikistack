c2ec91ce5919bf39c63fed7c23cea4f2bd9470a1
Raghava Kamalesh <raghava.kamalesh@gmail.com>
2018-04-27 11:54:03 -0400

    main test page working

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const router = express.Router();
+const index = require('./views/index')
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
 
-app.use(body.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get("/main", (req, res, next) => {
+  let test = index.main("")
+  res.send(test)
+});
 
 
 
+const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
